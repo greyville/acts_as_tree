5ab406972c68df5af5eeac64db6e572c3a952e26
Oliver Beddows <oliver.beddows@googlemail.com>
2014-06-27 14:05:24 +0100

    Fixing database connection syntax.

--- test/acts_as_tree_test.rb
+++ test/acts_as_tree_test.rb
@@ -19,7 +19,7 @@ class Test::Unit::TestCase
   end
 end
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
+ActiveRecord::Base.establish_connection(:database => "acts_as_tree", :adapter => "sqlite3", :dbfile => ":memory:")
 
 # AR keeps printing annoying schema statements
 $stdout = StringIO.new
